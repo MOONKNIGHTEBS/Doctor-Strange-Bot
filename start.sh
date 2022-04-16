d433fb78ad99f56db7d6ce51000a82fc7c88be97
Thorappan-TG <103317425+Thorappan-TG@users.noreply.github.com>
2022-04-16 16:04:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kckhais/Eva-KC-Bot.git /EvaMaria
+  git clone https://github.com/Thorappan-TG/Doctor-Strange-Bot.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "🔥#######🔥THE DOCTOR STRANGE WAS STARTED🔥#######🔥"
 python3 bot.py
